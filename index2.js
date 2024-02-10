a2902cb1838217e9395dd42fae7fd4df27aa9ba0
Ashraf Khan <85683933+trickedcupid@users.noreply.github.com>
2024-02-11 01:11:56 +0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // To parse JSON bodies
 app.use(bodyParser.json())
